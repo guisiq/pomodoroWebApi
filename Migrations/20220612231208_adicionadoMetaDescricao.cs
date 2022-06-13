ffaddc501dfdfc06e1bd34ba4c803aa5243b4bc9
henriqueguilherme <henriqueguilherme@unitins.com>
2022-06-12 22:23:16 -0300

    adicionando descricao em meta

---
+++ Migrations/20220612231208_adicionadoMetaDescricao.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pomodoro.Migrations
+{
+    public partial class adicionadoMetaDescricao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "descricao",
+                table: "Metas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "descricao",
+                table: "Metas");
+        }
+    }
+}
